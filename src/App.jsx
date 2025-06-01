dd42aca2acbbdad91bd54eadb5077ff89e3ed989
Hasitha Rukmal <rukmal7@yahoo.com>
2025-06-01 19:51:05 +0530

    appwrite integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
+import { useDebounce } from "react-use";
 import Search from "./components/Search/Search.componet";
 import Spinner from "./components/Search/Spinner/Spinner.components";
+import MovieCard from "./components/MovieCard.component";
+import { client, account, databases as database } from "./lib/appwrite";
+import { ID, Query } from "appwrite";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -12,26 +16,67 @@ const API_OPTIONS = {
   },
 };
 
-/*
+const DATBASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
 
+const updateSearchCount = async (searchTerm, movie) => {
+  try {
+    // check if search term already exists
+    const result = await database.listDocuments(DATBASE_ID, COLLECTION_ID, [
+      Query.equal("searchTerm", searchTerm),
+    ]);
 
-fetch(url, options)
-  .then(res => res.json())
-  .then(json => console.log(json))
-  .catch(err => console.error(err));
-*/
+    if (result.documents.length > 0) {
+      // if yes, update the document
+      const doc = result.documents[0];
+      await database.updateDocument(DATBASE_ID, COLLECTION_ID, doc.$id, {
+        count: doc.count + 1,
+      });
+    } else {
+      // if not, create a new document
+      await database.createDocument(DATBASE_ID, COLLECTION_ID, ID.unique(), {
+        searchTerm: searchTerm,
+        count: 1,
+        poster_url: `https://image.tmdb.org/t/p/w500/${movie?.poster_path}`,
+        movie_id: movie?.id,
+      });
+    }
+  } catch (error) {
+    console.log("Error updating metrics:", error);
+  }
+};
+
+const getTrendingMovies = async () => {
+  try {
+    const result = await database.listDocuments(DATBASE_ID, COLLECTION_ID, [
+      Query.limit(5),
+      Query.orderDesc("count"),
+    ]);
+
+    return result.documents;
+  } catch (error) {
+    console.log("Error fetchin trending movies:", error);
+  }
+};
+
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [errors, setErrors] = useState(null);
   const [movies, setMovies] = useState([]);
+  const [trendingMovies, setTrendingMovies] = useState([]);
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("");
 
-  const fetchMovoes = async () => {
+  useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm]);
+
+  const fetchMovies = async (query) => {
     setIsLoading(true);
     setErrors(null);
 
     try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
       const response = await fetch(endpoint, API_OPTIONS);
       if (!response.ok) {
@@ -46,6 +91,10 @@ function App() {
       }
 
       setMovies(data.results || []);
+
+      if (query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
     } catch (error) {
       console.error(`Error fetching movies: ${error}`);
       setErrors(error);
@@ -55,7 +104,16 @@ function App() {
   };
 
   useEffect(() => {
-    fetchMovoes();
+    fetchMovies(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
+
+  useEffect(() => {
+    const fetchTrendingMovies = async () => {
+      const result = await getTrendingMovies();
+      setTrendingMovies(result);
+    };
+
+    fetchTrendingMovies();
   }, []);
 
   return (
@@ -73,8 +131,22 @@ function App() {
           <Search searchterm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
 
+        {trendingMovies.length > 0 && (
+          <section className="ternding">
+            <h2>Trending Movies</h2>
+            <ul>
+              {trendingMovies.map((movie, index) => (
+                <li key={movie?.$id}>
+                  <p>{index + 1}</p>
+                  <img src={movie?.poster_url} alt={movie?.title} />
+                </li>
+              ))}
+            </ul>
+          </section>
+        )}
+
         <section className="all-movies">
-          <h2 className="mt-[40px]">All Movies</h2>
+          <h2>All Movies</h2>
 
           {isLoading ? (
             <Spinner />
@@ -83,13 +155,7 @@ function App() {
           ) : (
             <ul>
               {movies.map((movie) => (
-                <li key={movie.id} className="text-white">
-                  <img
-                    src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                    alt={movie.title}
-                  />
-                  <p>{movie.title}</p>
-                </li>
+                <MovieCard key={movie.id} movie={movie} />
               ))}
             </ul>
           )}
